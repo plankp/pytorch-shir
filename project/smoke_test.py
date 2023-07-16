de16c7a9a23f925fffcfdf6c93ab8b6d268e6523
Paul Teng <plankp@outlook.com>
2023-07-16 05:32:33 -0400

    Bump smoke test

--- project/smoke_test.py
+++ project/smoke_test.py
@@ -1,20 +1,22 @@
 import torch
 import shir_backend
+import shir_intrinsic
 
 @torch.compile(backend=shir_backend.compiler)
-def fn(x, y):
-  return torch.ops.aten.convolution.default(
-    x, y,
-    torch.zeros(4, dtype=torch.int),
-    [1, 1],
-    [0, 0],
-    [1, 1],
-    False,
-    [0, 0],
-    1
-)
+def fn(data, kern, bias):
+  return torch.ops.aten.convolution(
+      data,
+      kern,
+      bias,
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      False,
+      [0, 0],
+      1
+  )
 
-print(fn(
-    torch.ones(9, 2, 5, 5, dtype=torch.int),
-    torch.ones(4, 2, 3, 3, dtype=torch.int)
-).shape)
+data = (torch.randn(1, 1, 10, 10) * 100).to(torch.int8)
+kern = (torch.randn(2, 1, 3, 3) * 100).to(torch.int8)
+bias = (torch.ones(2)).to(torch.int8)
+print(fn(data, kern, bias).shape)
